4b3098a3df9a879015cda766cb3541f0ab86b24b
snaveed1 <159036395+snaveed1@users.noreply.github.com>
2024-10-17 16:16:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,36 @@ const port = 30006;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('WOHLIG TRANSFORMATIONS FROM APP6.\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>App6</title>
+            <style>
+                body {
+                    background-color: lightgrey;
+                    margin: 0;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                    font-size: 2em;
+                    font-weight: bold;
+                    text-align: left;
+                }
+                div {
+                    margin-left: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <div>END OF PROJECT!!! FROM APP6.</div>
+        </body>
+        </html>
+    `);
 });
 
 server.listen(port, hostname, () => {
